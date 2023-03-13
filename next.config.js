ab2d1fe576d58d7344d524ff64ac3e528f49b20f
leoplct <leonardo.pellicciotta@gmail.com>
2023-03-13 16:48:06 +0100

    boh

--- next.config.js
+++ next.config.js
@@ -7,16 +7,6 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
-    localeDetection: true,
-    // These are all the locales you want to support in
-    locales: ['en', 'it'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'en',
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com"
-  },
 }
 
 module.exports = withMarkdoc({ schemaPath: './src/pages' })(nextConfig)
